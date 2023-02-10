4dea68c2385bef20a31d04317b12f44446216f5c
Harshil Tagline <hsb@taglineinfotech.com>
2023-02-10 17:07:33 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/react-vite-pwa/",
+  base: "/react-vite-pwa/",
   server: {
     port: 8080,
     hot: true,
